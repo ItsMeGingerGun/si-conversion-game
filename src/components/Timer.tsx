27405169947179ea24dcef587978d36b7d74eef6
ItsMeGingerGun <happysolutionsivakasi@gmail.com>
2025-07-09 23:34:34 +0530

    Update Timer.tsx

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react';
+'use client'; // Add this at the very top
 
+import { useEffect, useState } from 'react';
 export default function Timer({ 
   duration, 
   onEnd 
