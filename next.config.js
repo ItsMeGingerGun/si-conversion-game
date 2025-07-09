fb87c0b949b284fc80b17959d7365606c0b25ac4
ItsMeGingerGun <happysolutionsivakasi@gmail.com>
2025-07-09 23:41:27 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
+const path = require('path');
+
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['picsum.photos'],
   },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  }
 };
