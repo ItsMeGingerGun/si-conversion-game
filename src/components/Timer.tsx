f253790c628d5b0b599da98b6b61294aa8f63913
ItsMeGingerGun <happysolutionsivakasi@gmail.com>
2025-07-09 22:56:28 +0530

    Create Timer.tsx

---
+++ src/components/Timer.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+export default function Timer({ 
+  duration, 
+  onEnd 
+}: { 
+  duration: number; 
+  onEnd: () => void 
+}) {
+  const [timeLeft, setTimeLeft] = useState(duration / 1000);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(prev => {
+        if (prev <= 0.1) {
+          clearInterval(timer);
+          onEnd();
+          return 0;
+        }
+        return prev - 0.1;
+      });
+    }, 100);
+
+    return () => clearInterval(timer);
+  }, [onEnd]);
+
+  return <div>Time: {timeLeft.toFixed(1)}s</div>;
+}
